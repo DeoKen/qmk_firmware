93300361182088e8e37698fd5b69d06e3a0e7e27
KennethDC <deoragekiss@gmail.com>
2025-03-12 15:37:20 -0400

    Update config.h

--- keyboards/sofle/keymaps/DeoKen/config.h
+++ keyboards/sofle/keymaps/DeoKen/config.h
@@ -6,7 +6,7 @@
 /* Select hand configuration */
 
 ///https://thomasbaart.nl/2018/12/01/reducing-firmware-size-in-qmk/
-// #define EE_HANDS
+#define EE_HANDS
 
 #define RP2040_FLASH_GD25Q64CS
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED D5
@@ -21,21 +21,21 @@
     #define TAPPING_TERM 200
 #endif
 #define ENCODER_DIRECTION_FLIP
-
+#define FORCE_NKRO
 
 #define RGBLIGHT_SLEEP
 //
 #define RGBLIGHT_LAYERS
 
 /* ws2812 RGB LED */
-#define WS2812_DI_PIN D4
+// #define WS2812_DI_PIN D4
 
 
-// #ifdef RGB_MATRIX_ENABLE
-// #define RGBLIGHT_LED_COUNT 35    // Number of LEDs
-// #define RGBLIGHT_LED_COUNT 35    // Number of LEDs
-// #define RGB_MATRIX_LED_COUNT RGBLIGHT_LED_COUNT
-// #endif
+#ifdef RGB_MATRIX_ENABLE
+#define RGBLIGHT_LED_COUNT 35    // Number of LEDs
+#define RGBLIGHT_LED_COUNT 35    // Number of LEDs
+#define RGB_MATRIX_LED_COUNT RGBLIGHT_LED_COUNT
+#endif
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLIGHT_LED_COUNT
@@ -59,25 +59,25 @@
     #undef RGBLIGHT_LIMIT_VAL
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_SAT_STEP 10
+    #define RGBLIGHT_VAL_STEP 10
 #endif
 
-// #ifdef RGB_MATRIX_ENABLE
-// #   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-// // #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
-// #   define RGB_MATRIX_SLEEP // turn off effects when suspended
-// #   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-// // #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// // #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-// #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
+#ifdef RGB_MATRIX_ENABLE
+#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
+#   define RGB_MATRIX_SLEEP // turn off effects when suspended
+#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+// #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
+// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
+#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 
-// #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_GRADIENT_LEFT_RIGHT
 
-// #    define RGB_MATRIX_HUE_STEP 8
-// #    define RGB_MATRIX_SAT_STEP 8
-// #    define RGB_MATRIX_VAL_STEP 8
-// #    define RGB_MATRIX_SPD_STEP 10
+#    define RGB_MATRIX_HUE_STEP 8
+#    define RGB_MATRIX_SAT_STEP 8
+#    define RGB_MATRIX_VAL_STEP 8
+#    define RGB_MATRIX_SPD_STEP 10
 
 /* Disable the animations you don't want/need.  You will need to disable a good number of these    *
  * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
@@ -109,6 +109,6 @@
  // #   undef ENABLE_RGB_MATRIX_MULTISPLASH
  // #   undef ENABLE_RGB_MATRIX_SOLID_SPLASH
  // #   undef ENABLE_RGB_MATRIX_SOLID_MULTISPLASH
-// #endif
-#define OLED_TIMEOUT 60000
+#endif
+#define OLED_TIMEOUT 3600000
 #define OLED_SCROLL_TIMEOUT 30000
